f4b28a8227663878a0a800819e5215c529f82031
avahodge <20hodgea15@stu.smuhsd.org>
2018-08-20 09:46:30 -0700

    First project to commit to github (2018)

---
+++ Hello_world/src/Hello__world.java
@@ -0,0 +1,13 @@
+/**
+ * Will print out text saying "Hello world!"
+ * 
+ * @author Ava Hodge 
+ * @version 8/20/18
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Ava Hodge"); //Display the string
+    }
+}
