4b20beadd0f30c7351a1cd1668df172877a0cd43
avahodge <20hodgea15@stu.smuhsd.org>
2018-09-05 09:48:41 -0700

    pika pii

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,14 @@
+
+public class Pikachu {
+public static void main (String[] args) {
+//    Pikachu welcomes you to the world of Pokemon!
+//  (\__/)
+//  (o^.^)
+//z(_(")(")
+// Ava Hodge, 8/30/18
+	System.out.println ("Pikachu welcomes you to the world of Pokemon!");
+	System.out.println ("\t\t     (\\__/)");
+	System.out.println ("\t\t     (o^.^)");
+	System.out.println ("\t           z(_(\")(\")");  
+	}
+}
